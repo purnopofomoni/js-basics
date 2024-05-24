5e27a12c31a6d69570379de729e6b00548a1fb31
Purnachandra <78591733+purnopofomoni@users.noreply.github.com>
2024-05-24 06:29:32 +0000

    variables practice

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 123456
+let accountName = "Purna"
+var accountEmail = "purna@gmail.com"
+accountType = "savings"
+
+accountName = "Chandra"
+accountEmail = "chandra@gmaiol.com"
+accountType="current"
+
+//accountId = 553876
+console.table([accountId,accountName,accountEmail,accountType])
